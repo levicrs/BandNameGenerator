e5369dff8e983659103cbcf986b66a7fcffb2320
leviserrano <leviserrano@outlook.pt>
2021-11-01 19:11:04 +0000

    no message

---
+++ main.py
@@ -0,0 +1,22 @@
+# dia 1 Band Name Generator
+# Levi Serrano@01-11-2021
+
+print(" ___   ___            _                       ___              ___\n"
+      "|   \ |   | |\     | | \            |\     | |   | |\      /| |\n"
+      "|   | |   | | \    | |  \           | \    | |   | | \    / | |\n"
+      "| _/  |___| |  \   | |   |   ____   |  \   | |___| |  \  /  | |__\n"
+      "|  \  |   | |   \  | |   |  |____|  |   \  | |   | |   \/   | |\n"
+      "|   | |   | |    \ | |  /           |    \ | |   | |        | |\n"
+      "|_ /  |   | |     \| |_/            |     \| |   | |        | |___")
+
+borned_city = input("In which city have you born ?\n")
+
+pet_name = input("Tell me a pet name that was important to you...\n")
+
+print("Your band could be named " + borned_city + " " + pet_name )
+
+
+
+
+
+
